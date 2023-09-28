16ee35bca9a42ddba0fdc9ad33985deeb59ff964
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-28 14:35:34 +0500

    add babel

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button =({text, styleClass, name, type, otherprops, onClick})=> {
+const Button =(props)=> {
+  const {text, styleClass, name, type, otherprops, onClick} = props;
   console.log("Button Props ==== ", text, styleClass, name, type, otherprops);
+
+    return (
+      <>
+        <Button >{text}</Button>
+        {/* <Button name={name} type={type} className={`btn btn-${styleClass}`}  onClick={onClick}>{text}</Button> */}
+      </>
+    )
   
-  return (
-    <>
-      <Button >{text}</Button>
-      {/* <Button name={name} type={type} className={`btn btn-${styleClass}`}  onClick={onClick}>{text}</Button> */}
-    </>
-  )
 }
 
-Button.propTypes = {
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  style: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
-}
+// Button.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   type: PropTypes.string.isRequired,
+//   text: PropTypes.string.isRequired,
+//   style: PropTypes.string,
+//   onClick: PropTypes.func.isRequired,
+// }
 
 export default Button;
