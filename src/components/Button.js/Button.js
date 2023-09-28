2ca1c4c241b052d7d055e1621d0a744a03325a03
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-28 12:29:03 +0500

    Button Comp. update

--- src/components/Button.js/Button.js
+++ src/components/Button.js/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const Button = () => {
+  console.log("Button Props ==== ", props);
   return (
     <>
-        <Button class={props.class} name={props.name} type={props.type} {...otherprops}>{props.text}</Button>
+        <Button class={props.class} name={props.name} type={props.type} onClick={props.onClick} {...otherprops}>{props.text}</Button>
     </>
   )
 }
