358950fe8c686d8eb481f907e0f02bcf895c3fe2
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 17:38:56 +0500

    import Card Comp. in wrapper

--- src/components/Todo/TodoWrapper.js
+++ src/components/Todo/TodoWrapper.js
@@ -1,11 +1,16 @@
 import React from "react";
 import TodoForm from "./TodoForm";
+import CardComp from "../CardComp/CardComp";
 
 const TodoWrapper = () => {
   return (
     <>
       <div className="todowrapper">
-        <TodoForm></TodoForm>
+        <CardComp>
+
+        </CardComp>
+        {/* <TodoForm></TodoForm> */}
+
       </div>
     </>
   );
