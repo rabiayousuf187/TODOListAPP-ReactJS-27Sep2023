8424a4235ca3ff53b084e7e7fdb4e03f147e7f01
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 19:09:21 +0500

    set previous and current TODO

--- src/components/Todo/TodoWrapper.js
+++ src/components/Todo/TodoWrapper.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import CardComp from "../CardComp/CardComp";
+import { v4 as uuidv4 } from "uuid";
+
+// for Universal Unique Identifier-128
+uuidv4();
 
 const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodos = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+
+    console.log("todos === ", todos);
+  };
+
   return (
     <>
       <div className="todowrapper">
         <CardComp>
-
+          <TodoForm addTodos={addTodos}></TodoForm>
         </CardComp>
-        {/* <TodoForm></TodoForm> */}
-
       </div>
     </>
   );
