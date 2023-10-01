e1d833cd374ee53a6de61f86b3202fe6fb3f99b0
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 21:16:19 +0500

    worked on completed task

--- src/components/Todo/TodoWrapper.js
+++ src/components/Todo/TodoWrapper.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import CardComp from "../CardComp/CardComp";
-import TodoList from './TodoList'
+import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 
 // for Universal Unique Identifier-128
@@ -19,6 +19,13 @@ const TodoWrapper = () => {
     console.log("todos === ", todos);
   };
 
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo;
+      })
+    );
+  };
   return (
     <>
       <div className="todowrapper">
@@ -26,8 +33,14 @@ const TodoWrapper = () => {
           <TodoForm addTodos={addTodos}></TodoForm>
         </CardComp>
 
-        {todos.map((todo, ind)=>{
-           return <TodoList task={todo} key={ind}></TodoList>
+        {todos.map((todo, ind) => {
+          return (
+            <TodoList
+              task={todo}
+              key={ind}
+              toggleComplete={toggleComplete}
+            ></TodoList>
+          );
         })}
       </div>
     </>
