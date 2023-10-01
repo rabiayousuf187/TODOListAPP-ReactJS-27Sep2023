b15dec4341a18e6deeaec458abe591b5ce1d912b
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 19:10:01 +0500

    setTodo current value and reset feild again

--- src/components/Todo/TodoForm.js
+++ src/components/Todo/TodoForm.js
@@ -1,29 +1,33 @@
 import React, { useState } from "react";
 import { Form, Row, Col, Input } from "reactstrap";
-import ButtonComp from "../Button/Button";
+import ButtonComp from "../ButtonComp/ButtonComp";
 
-const TodoForm = () => {
+const TodoForm = ({addTodos}) => {
     const [value, setValue] = useState([]);
     let handleSubmit = (event) =>{
         event.preventDefault();
 
+        addTodos(value);
+        setValue("")
         console.log(event.target.value);
     }
   return (
     <>
       <Form id="todoForm" onSubmit={handleSubmit}>
-        <Row className="row-cols-lg-auto g-3 align-items-center">
-          <Col>
+        <Row className="row-cols-lg-auto g-3 justify-content-center align-items-center">
+          <Col lg={10}>
             <Input
               id="todo"
               name="todo"
               placeholder="What is the task todo?"
               type="text"
-              onChange={(e)=>{ setValue(e.target.value) }}
+              value={value}
+              onChange={(e)=>{setValue(e.target.value)}}
+            //   onChange={(e)=>{ e.target.value != "" ? setValue(e.target.value) : <AlertComp ""/>}}
             />
           </Col>
-          <Col>
-            <ButtonComp text="Add" name='add' type='submit'></ButtonComp>
+          <Col lg={2}>
+            <ButtonComp text="Add" name='add' type='submit' id="add"></ButtonComp>
           </Col>
         </Row>
       </Form>
