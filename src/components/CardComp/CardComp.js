6453fa305ea72faea5fc4318ede763f8ef89cad1
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 22:28:51 +0500

    optimize code

--- src/components/CardComp/CardComp.js
+++ src/components/CardComp/CardComp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle, CardText } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 import TodoForm from "../Todo/TodoForm";
 
 const CardComp = (props) => {
