b3870d7d4d0872440551439470f2eb04c97d33a5
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 15:25:57 +0500

    customize Button Comp.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "reactstrap";
 
-const Button =(props)=> {
-  const {text, styleClass, name, type, otherprops, onClick} = props;
-  console.log("Button Props ==== ", text, styleClass, name, type, otherprops);
+const ButtonComp = ({ text, color, className, name, type, onClick }) => {
 
-    return (
-      <>
-        <Button >{text}</Button>
-        {/* <Button name={name} type={type} className={`btn btn-${styleClass}`}  onClick={onClick}>{text}</Button> */}
-      </>
-    )
-  
-}
+  console.log("Button Props ==== ", text, color, className, name, type, onClick);
+  color === undefined ? color ='primary' : color = color;
+  return (
+    <>
+      <Button color={color} size="" onClick={onClick}>{text}</Button>
+      
+      {/* <Button onClick={onClick}>{props.text}</Button> */}
+      {/* <Button onClick={onClick}>{text}</Button> */}
+      {/* <Button name={name} type={type} className={`btn btn-${styleClass}`}  onClick={onClick}>{text}</Button> */}
+    </>
+  );
+};
 
-// Button.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   type: PropTypes.string.isRequired,
-//   text: PropTypes.string.isRequired,
-//   style: PropTypes.string,
-//   onClick: PropTypes.func.isRequired,
-// }
+ButtonComp.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  style: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+};
 
-export default Button;
+export default ButtonComp;
