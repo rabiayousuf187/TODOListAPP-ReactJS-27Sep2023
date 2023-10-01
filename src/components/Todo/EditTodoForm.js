3a036f02d4946db5ff42a10367c797350e6e2b55
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 21:33:32 +0500

    Edit Form created

---
+++ src/components/Todo/EditTodoForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Form, Row, Col, Input } from "reactstrap";
+import ButtonComp from "../ButtonComp/ButtonComp";
+
+const EditTodoForm = ({editTodos, task}) => {
+    const [value, setValue] = useState([]);
+    let handleSubmit = (event) =>{
+        event.preventDefault();
+
+        editTodos(value, task.id);
+        setValue("")
+        console.log(event.target.value);
+    }
+  return (
+    <>
+      <Form id="todoForm" onSubmit={handleSubmit}>
+        <Row className="row-cols-lg-auto g-3 justify-content-center align-items-center">
+          <Col lg={10}>
+            <Input
+              id="todo"
+              name="todo"
+              placeholder="Update Task"
+              type="text"
+              value={value}
+              onChange={(e)=>{setValue(e.target.value)}}
+            //   onChange={(e)=>{ e.target.value != "" ? setValue(e.target.value) : <AlertComp ""/>}}
+            />
+          </Col>
+          <Col lg={2}>
+            <ButtonComp text="Update" name='add' type='submit' id="update"></ButtonComp>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
+export default EditTodoForm;
