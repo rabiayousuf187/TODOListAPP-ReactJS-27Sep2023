62b2b98795828d55bb52ed1726b77c25f0ecde2d
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-01 16:44:41 +0500

    TodoWrapper created

---
+++ src/components/Todo/TodoWrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+
+const TodoWrapper = () => {
+  return (
+    <>
+      <TodoForm>
+        
+      </TodoForm>
+    </>
+  );
+};
+
+export default TodoWrapper;
